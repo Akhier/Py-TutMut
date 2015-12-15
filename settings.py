30f4fd90e53c3161ddd777e363f66c41622821c3
akhier <akherthedragonhearted@gmail.com>
2015-12-15 13:54:11 -0500

    Setting up the globals and settings

---
+++ settings.py
@@ -0,0 +1,52 @@
+import libtcodpy as libtcod
+
+
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+
+MAP_WIDTH = 80
+MAP_HEIGHT = 43
+
+BAR_WIDTH = 20
+PANEL_HEIGHT = 7
+PANEL_Y = SCREEN_HEIGHT - PANEL_HEIGHT
+MSG_X = BAR_WIDTH + 2
+MSG_WIDTH = SCREEN_WIDTH - BAR_WIDTH - 2
+MSG_HEIGHT = PANEL_HEIGHT - 1
+INVENTORY_WIDTH = 50
+CHARACTER_SCREEN_WIDTH = 30
+LEVEL_SCREEN_WIDTH = 40
+
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
+
+HEAL_AMOUNT = 40
+LIGHTNING_DAMAGE = 40
+LIGHTNING_RANGE = 5
+CONFUSE_RANGE = 8
+CONFUSE_NUM_TURNS = 10
+FIREBALL_RADIUS = 3
+FIREBALL_DAMAGE = 25
+
+LEVEL_UP_BASE = 200
+LEVEL_UP_FACTOR = 150
+
+FOV_ALGO = 0
+FOV_LIGHT_WALLS = True
+TORCH_RADIUS = 10
+
+LIMIT_FPS = 20
+
+
+def init():
+    global color_dark_ground, color_light_ground, \
+        color_dark_wall, color_light_wall
+    color_dark_wall = libtcod.Color(0, 0, 100)
+    color_light_wall = libtcod.Color(130, 110, 50)
+    color_dark_ground = libtcod.Color(50, 50, 150)
+    color_light_ground = libtcod.Color(200, 180, 50)
+
+    global dungeon_level, fov_map, fov_recompute, \
+        game_msgs, game_state, inventory, key, \
+        map, mouse, objects, player, stairs
